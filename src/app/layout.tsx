ce6d64337bde06970a7b2a5f6b14a0c9a93e81cb
cheshire <cheshire.park@h2ohospitality.io>
2025-08-12 15:07:35 +0900

    chore: favicon 및 og 이미지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,19 +17,29 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* font */}
         <link
           rel="stylesheet"
           as="style"
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
         />
+
+        {/* OG meta tags */}
         <meta property="og:title" content="휴렌터카" />
         <meta property="og:description" content="신차 장기렌트 전문" />
-        <meta property="og:image" content="/images/og-image.png" />
+        <meta property="og:image" content="/images/og-image.jpeg" />
         <meta property="og:url" content="https://hue-car-rental.vercel.app" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="ko_KR" />
         <meta property="og:site_name" content="휴렌터카" />
+
+        {/* favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </head>
       <body className="antialiased relative">
         <Header />
