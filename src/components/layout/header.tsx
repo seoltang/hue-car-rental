aa6ecbbfcb4c1377b0d745c2cfc3145c06e3b158
cheshire <cheshire.park@h2ohospitality.io>
2025-08-20 15:07:07 +0900

    feat: 헤더 클릭 시 스크롤 맨 위로 이동 기능 추가

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,6 +8,10 @@ import { cn } from '@/styles/utils';
 const Header = () => {
   const [visible, setVisible] = useState(false);
 
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0 });
+  };
+
   useEffect(() => {
     let ticking = false;
     const onScroll = () => {
@@ -26,11 +30,12 @@ const Header = () => {
   return (
     <header
       className={cn(
-        'fixed top-0 inset-x-0 z-50 w-full h-header-height px-12 py-3.5 xl:py-[1.125rem] inline-flex justify-center items-center bg-white transition-all duration-300',
+        'fixed top-0 inset-x-0 z-50 w-full h-header-height px-12 py-3.5 xl:py-[1.125rem] inline-flex justify-center items-center bg-white transition-all duration-300 cursor-pointer',
         visible
           ? 'bg-white shadow-[0px_2.4px_12px_0px_rgba(0,0,0,0.08)]'
           : 'bg-transparent shadow-none',
       )}
+      onClick={handleScrollToTop}
     >
       <Image src={logo} alt="logo" width={64} height={36} className="w-auto h-full" />
     </header>
