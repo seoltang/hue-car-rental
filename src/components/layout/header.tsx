45a38662a43c45c4cea6896f73b274d6702b2b84
cheshire <cheshire.park@h2ohospitality.io>
2025-08-20 14:56:39 +0900

    fix: 데스크탑 버전 header 높이 수정

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -26,13 +26,13 @@ const Header = () => {
   return (
     <header
       className={cn(
-        'fixed top-0 inset-x-0 z-50 w-full h-header-height px-12 py-3.5 inline-flex flex-col justify-center items-center bg-white transition-all duration-300',
+        'fixed top-0 inset-x-0 z-50 w-full h-header-height px-12 py-3.5 xl:py-[1.125rem] inline-flex justify-center items-center bg-white transition-all duration-300',
         visible
           ? 'bg-white shadow-[0px_2.4px_12px_0px_rgba(0,0,0,0.08)]'
           : 'bg-transparent shadow-none',
       )}
     >
-      <Image src={logo} alt="logo" width={49} height={28} />
+      <Image src={logo} alt="logo" width={64} height={36} className="w-auto h-full" />
     </header>
   );
 };
